1f252b66e5ebe6cace66378151e7475fc81bc07d
gvp-edu <909425809@qq.com>
2024-05-14 20:22:21 +0800

    other(gateway): 其他修改

--- README.md
+++ README.md
@@ -5,11 +5,19 @@
 * https://www.51test.net/show/5854275.html
 
 
+## 项目相关
+
+* http相关常量使用org.springframework.http.HttpHeaders中提供的
+* 响应token可以放置在响应的响应头中
+
+### mysql日期时间
+* https://juejin.cn/post/7290382189992165415
+* 性能: bigint > datetime > timestamp
 
 
 ## 提交格式
 
-* 提交类型
+* 提交类型 
   * feat: 新功能
   * fix: 修复问题(bug)
   * docs: 修改文档
@@ -21,6 +29,7 @@
   * deps: 升级依赖
   * revert: 回滚版本
   * build: 构建系统或者包依赖更新
+  * other: 其他不重要修改
 * 范围Scope
     * 用于说明本次修改涉及的范围-模块
 * 主题 Subject
---
+++ summer-gateway/src/main/java/org/gvp/gateway/config/GatewayConfig.java
@@ -0,0 +1,28 @@
+package org.gvp.gateway.config;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.log4j.Log4j2;
+import org.gvp.gateway.security.SecurityProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+
+@Log4j2
+@Configuration
+@EnableConfigurationProperties
+public class GatewayConfig {
+    @Autowired
+    private SecurityProperties securityProperties;
+
+    @Autowired
+    private ReactiveRedisTemplate<String, Object> reactiveRedisTemplate;
+
+    @PostConstruct
+    public void init() {
+        log.debug("网关安全模块配置文件: {}", securityProperties);
+//        Mono<Boolean> test = this.reactiveRedisTemplate.hasKey("test");
+//        test.subscribe( e -> log.error("测试key[test],如果不测试会报错连接redis失败: {}",e) );
+
+    }
+}
